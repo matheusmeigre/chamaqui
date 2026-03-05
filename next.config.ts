18d05181a5cb567593854968f36ab238404f5f51
Matheus Meigre <matheusmeigre@hotmail.com>
2026-03-05 17:31:06 -0300

    fix: next config fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
